5794ea809e733acb9676191f3f3ce5da08dcf0ed
kdh <kd_hong@163.com>
2019-05-15 07:53:29 +0800

    add import

--- few_shot_learning/prototypical_network/core/main.py
+++ few_shot_learning/prototypical_network/core/main.py
@@ -1,9 +1,9 @@
 
 import os
 import sys
-sys.path.append(os.path.dirname(__file__))
+sys.path.append()
 sys.path.append('../../../')
-print(os.path.dirname('../../../'))
+print(os.path.dirname(os.path.dirname(os.path.dirname(os.path.dirname(__file__)))))
 sys.path.append(os.path.dirname('..'))
 
 
